6eed0c76c25dee7d2b4adcf5adcf769103f14d8e
yann-fk-21 <168545866+yann-fk-21@users.noreply.github.com>
2024-09-18 11:30:59 +0100

    Initial commit

---
+++ server/jest.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  clearMocks: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  setupFiles: ["dotenv/config"],
+};
